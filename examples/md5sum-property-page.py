8ee9260be3924b17de1fa244fb963a5724c3d788
Codeform <pagini_yueu@outlook.com>
2020-03-28 18:01:01 +0800

    modify md5sum property page to: 1. allow multiple files to be checked; 2. allow checksum to be copied

--- examples/md5sum-property-page.py
+++ examples/md5sum-property-page.py
@@ -1,47 +1,39 @@
 import hashlib
-
-# A way to get unquote working with python 2 and 3
-try:
-    from urllib import unquote
-except ImportError:
-    from urllib.parse import unquote
-
+from urllib.parse import unquote
 from gi.repository import Nautilus, Gtk, GObject
+from os import path
+
 
 class MD5SumPropertyPage(GObject.GObject, Nautilus.PropertyPageProvider):
     def __init__(self):
         pass
-    
+
     def get_property_pages(self, files):
-        if len(files) != 1:
-            return
-        
-        file = files[0]
-        if file.get_uri_scheme() != 'file':
-            return
-
-        if file.is_directory():
-            return
-
-        filename = unquote(file.get_uri()[7:])
-
-        self.property_label = Gtk.Label('MD5Sum')
+        self.property_label = Gtk.Label('MD5 Sum')
         self.property_label.show()
 
-        self.hbox = Gtk.HBox(homogeneous=False, spacing=0)
-        self.hbox.show()
+        self.contents = Gtk.ScrolledWindow(min_content_height=200)
+        list_box = Gtk.ListBox(selection_mode=Gtk.SelectionMode.NONE)
+        self.contents.add(list_box)
 
-        label = Gtk.Label('MD5Sum:')
-        label.show()
-        self.hbox.pack_start(label, False, False, 0)
+        for i in range(len(files)):
+            file = files[i]
+            if file.get_uri_scheme() != 'file' or file.is_directory():
+                continue
 
-        self.value_label = Gtk.Label()
-        self.hbox.pack_start(self.value_label, False, False, 0)
+            filename = unquote(file.get_uri()[len('file://'):])
+            md5sum = hashlib.md5(filename.encode("utf-8")).hexdigest()
 
-        md5sum = hashlib.md5(filename.encode("utf-8")).hexdigest()
-        self.value_label.set_text(md5sum)
-        self.value_label.show()
-        
+            label_fn = Gtk.Label(path.basename(filename))
+
+            label_md5 = Gtk.Label(md5sum, selectable=True)
+            entry_box = Gtk.HBox(spacing=20)
+            entry_box.pack_start(label_fn, False, False, 0)
+            entry_box.pack_start(label_md5, False, False, 0)
+            list_box.insert(entry_box, -1)
+
+        self.contents.show_all()
+
         return Nautilus.PropertyPage(name="NautilusPython::md5_sum",
-                                     label=self.property_label, 
-                                     page=self.hbox),
+                                     label=self.property_label,
+                                     page=self.contents),
